c73a29ccd25e841559653c58a84f75cdf8e738e8
BaaqsSi <alahomsa9@gmail.com>
2024-07-28 22:51:29 +0400

    second commit

---
+++ group 34/day 34/homework/i.py
@@ -0,0 +1,68 @@
+# def find_short(s):
+#     list1 = s.split(" ")
+
+
+#     word_len = len(list1[0])
+#     for i in list1:
+#         if len(i) < word_len:
+#             word_len = len(i)
+    
+#     # word_len = 3
+#     return word_len
+
+# print(find_short("bitcoin take over the world maybe who knows perhaps"))
+
+
+#mokled funqcias jer vqmnit mere mag funqcias gadavcemt s variables tu rac qvia da shemdeg magas vsplitavt da variableshi vatavsebt
+#shemdeg am gasplitul listshi vighebt 0 indexze myof sityvas da vuvlit iteraciit yvela matgans da yvelaze pataras varchevt es sheileba gavaumjobesot kidec:
+
+
+
+# def find_short(s):
+#     list1 = s.split(" ")
+
+#     return min(len(list) for list in list1)
+
+
+# print(find_short("bitcoin take over the world maybe who knows perhaps"))
+
+#am kodshi vighebt len funqciit yvela elements listshi da vabrunebt yvelaze pataras min() funciis gamoyenebit 😉😉
+
+
+
+# str1 = "apple,banana,cherry"
+# arr1 = str1.split(",")
+# print(arr1)
+
+
+# time = "2024-07-28"
+# time = time.split("-")
+# print(time)
+
+
+
+# sos = "suck/suck/sck"
+# sos = sos.split("/")
+# print(sos)
+
+
+
+# mail= "alahomsa9@gmail.com"
+# mail = mail.split("@")
+# print(mail)
+
+
+
+# names = "gio gio gi ogi o gio"
+
+# names = names.split(" ")
+
+# print(names)
+
+
+
+# users = "doggo dog dooggg"
+
+# users = users.split("g")
+
+# print(users)
