63cb28f6c1846954c1d549c39c3041c30a9167fd
BaaqsSi <alahomsa9@gmail.com>
2024-10-12 19:25:47 +0400

    Second commit

---
+++ group 34/level 62/classwork/script.js
@@ -0,0 +1,20 @@
+// 1
+
+function name(){
+    let myName = "luka lomidze";
+    console.log(myName);
+  }
+  
+name();
+
+
+// 2
+
+function butt(){
+    alert("baksibaksi");
+}
+
+// 3
+
+let yourName = prompt('Enter your name: ');
+console.log(yourName);
