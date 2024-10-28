d81fe25a9cb3ea3a3c4af574ec30b5ff8b4a10cb
BaaqsSi <alahomsa9@gmail.com>
2024-10-28 23:34:06 +0400

    second commit

---
+++ group 34/level 69/classwork/script.js
@@ -0,0 +1,21 @@
+let array = [1,2,3,4,5,6,7];
+console.log(array.length);
+
+let string = ['luka', "puka", "niga"];
+console.log(string.toString());
+
+console.log(string.at(1));
+console.log(string.join(":)"));
+console.log(array.pop());
+console.log(string.push("baksi"));
+console.log(string.shift(0));
+console.log(string.unshift("baksi"));
+let array1 = delete array [1];
+console.log(array1);
+console.log(array.concat(string));
+console.log(string.indexOf("baksi"));
+console.log(array.lastIndexOf("3"));
+console.log(array.includes(10));
+console.log(string.sort());
+console.log(string.reverse());
+
